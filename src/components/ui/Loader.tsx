3af2f98f905bc8824001eb7e5589986f4e1ecc99
i9-9 <yvan.vrs@gmail.com>
2025-08-31 22:34:23 -0300

    loader styles better

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -4,7 +4,7 @@ export default function Loader() {
   return (
     <div className="fixed inset-0 bg-[#EBEBEB] z-50 flex items-center justify-center">
       <div className="flex flex-col items-center">
-        <h1 className="font-baskerville text-[#151714] text-4xl md:text-6xl tracking-[0.2em] animate-scale-slow">
+        <h1 className="font-baskerville text-[#151714] text-4xl md:text-6xl tracking-[3rem] animate-scale-slow">
           GRUPO FRALI
         </h1>
       </div>
